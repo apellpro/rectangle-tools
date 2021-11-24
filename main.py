5c1cc2b661c8c645c5b9c5aa9ef3919a9dd14bdc
iamapell <exhthx@mail.ru>
2021-11-24 17:55:05 +0700

    Added union function

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def intersection(x1, y1, x2, y2, x3, y3, x4, y4) -> int:
     return width * height
 
 
+def union(x1, y1, x2, y2, x3, y3, x4, y4) -> int:
+    """Площадь объединения прямоугольников"""
+    inter_area = intersection(**locals())
+    first_area = abs(x1 - x2) * abs(y1 - y2)
+    second_area = abs(x3 - x4) * abs(y3 - y4)
+    return first_area + second_area - inter_area
+
+
 def main():
     args = input('Введите 8 целых чисел (координаты): ')
     try:
